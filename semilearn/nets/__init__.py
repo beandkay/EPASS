e2c5191fe824ec5a8a0530dbb938a4bb460453e7
Binh Nguyen <tinyswish@gmail.com>
2022-09-20 13:44:08 +0900

    remove file

--- semilearn/nets/__init__.py
+++ semilearn/nets/__init__.py
@@ -1,4 +1,4 @@
-from .resnet import resnet50, resnet50_aux
+from .resnet import resnet50
 from .wrn import wrn_28_2, wrn_28_8, wrn_var_37_2
 from .vit import vit_base_patch16_224, vit_small_patch16_224, vit_small_patch2_32, vit_tiny_patch2_32, vit_base_patch16_96
 from .bert import bert_base_cased, bert_base_uncased
