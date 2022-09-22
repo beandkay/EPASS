f3c8725451a61e72f82d151fc3fc9fb9a25d973c
Binh Nguyen <tinyswish@gmail.com>
2022-09-22 14:11:09 +0900

    fix bug

--- semilearn/algorithms/cagul/cagul.py
+++ semilearn/algorithms/cagul/cagul.py
@@ -226,7 +226,7 @@ class CAGUL(AlgorithmBase):
             probs_x_ulb_w = self.call_hook("dist_align", "DistAlignHook", probs_x_ulb=probs_x_ulb_w, probs_x_lb=probs_x_lb_w)
 
             # calculate weight
-            mask = self.call_hook("masking", "MaskingHook", logits_x_lb=probs_x_lb_w, probs_x_lb_s=probs_x_lb_s, logits_x_ulb_w=probs_x_ulb_w, softmax_x_lb=False, softmax_x_ulb=False)
+            mask = self.call_hook("masking", "MaskingHook", logits_x_lb_w=probs_x_lb_w, logits_x_lb_s=probs_x_lb_s, logits_x_ulb_w=probs_x_ulb_w, softmax_x_lb=False, softmax_x_ulb=False)
 
             # generate unlabeled targets using pseudo label hook
             pseudo_label = self.call_hook("gen_ulb_targets", "PseudoLabelingHook", 
